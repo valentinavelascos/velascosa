7405a7182a444aacebf686efe47e681876ef3d94
Valentina Velasco <133036750+valentinavelascos@users.noreply.github.com>
2026-01-06 17:28:07 -0500

    Update iframe style to use JSX syntax

--- app/layout.js
+++ app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
           src="https://www.googletagmanager.com/ns.html?id=GTM-5MP98L2J"
           height="0" 
           width="0" 
-          style="display:none;visibility:hidden">
+          style={{ display: "none", visibility: "hidden" }}>
           </iframe>
         </noscript>
 
